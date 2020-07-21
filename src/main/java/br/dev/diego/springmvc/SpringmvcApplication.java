0a653eee7f78466ae0e7de7e2683de25ddce6d27
Diego Sousa <diegosous@gmail.com>
2020-07-21 17:18:37 -0300

    Criando operacao de instanciacao Categoria

--- src/main/java/br/dev/diego/springmvc/SpringmvcApplication.java
+++ src/main/java/br/dev/diego/springmvc/SpringmvcApplication.java
@@ -1,13 +1,31 @@
 package br.dev.diego.springmvc;
 
+import br.dev.diego.springmvc.domain.Categoria;
+import br.dev.diego.springmvc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringmvcApplication {
+public class SpringmvcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
--- src/main/java/br/dev/diego/springmvc/services/CategoriaService.java
+++ src/main/java/br/dev/diego/springmvc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import br.dev.diego.springmvc.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
