245554d8e1695e18636c2f0471fb510d43d225a1
Diego Sousa <diegosous@gmail.com>
2020-07-21 11:53:34 -0300

    Commit projeto inicial

---
+++ src/test/java/br/dev/diego/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.diego.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
