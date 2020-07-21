fe653cc694adde308efdb1e472d9ab0427c25153
Diego Sousa <diegosous@gmail.com>
2020-07-21 19:40:17 -0300

    Classe Produto e associacao @ManyToMany

---
+++ src/main/java/br/dev/diego/springmvc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.dev.diego.springmvc.repositories;
+
+
+import br.dev.diego.springmvc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
