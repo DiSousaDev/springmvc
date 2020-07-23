b87f281b671c7060c2f60f53e31e98ce6531745d
Diego Sousa <diegosous@gmail.com>
2020-07-22 21:27:53 -0300

    ItemPedido e ItemPedidoPk

---
+++ src/main/java/br/dev/diego/springmvc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package br.dev.diego.springmvc.repositories;
+
+
+import br.dev.diego.springmvc.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
