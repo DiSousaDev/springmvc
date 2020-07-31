67f22f35f530a0601e434da1001329e047928f82
Diego Sousa <diegosous@gmail.com>
2020-07-30 22:18:28 -0300

    Enviando producao

--- src/main/java/br/dev/diego/springmvc/config/DevConfig.java
+++ src/main/java/br/dev/diego/springmvc/config/DevConfig.java
@@ -2,6 +2,7 @@ package br.dev.diego.springmvc.config;
 
 import br.dev.diego.springmvc.services.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,14 +10,21 @@ import org.springframework.context.annotation.Profile;
 import java.text.ParseException;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
     private DbService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
     @Bean
     public boolean instantiateDatabase() throws ParseException{
+
+        if(!"create".equals(strategy)){
+            return false;
+        }
         dbService.instantiateTestDatabase();
         return true;
     }
