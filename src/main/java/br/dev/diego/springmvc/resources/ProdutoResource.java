a9a051dad85222bbcbc3e78945e2aecc62297ea6
Diego Sousa <diegosous@gmail.com>
2020-08-06 20:29:13 -0300

    Configuração inicial do Spring Security

--- src/main/java/br/dev/diego/springmvc/resources/ProdutoResource.java
+++ src/main/java/br/dev/diego/springmvc/resources/ProdutoResource.java
@@ -29,12 +29,12 @@ public class ProdutoResource {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Page<ProdutoDTO>> findPage(
-            @RequestParam(value = "nome", defaultValue = "0") String nome,
-            @RequestParam(value = "categorias", defaultValue = "0") String categorias,
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy){
+            @RequestParam(value="nome", defaultValue="") String nome,
+            @RequestParam(value="categorias", defaultValue="") String categorias,
+            @RequestParam(value="page", defaultValue="0") Integer page,
+            @RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+            @RequestParam(value="orderBy", defaultValue="nome") String orderBy,
+            @RequestParam(value="direction", defaultValue="ASC") String direction) {
         String nomeDecoded = URL.decodeParam(nome);
         List<Integer> ids = URL.decodeIntList(categorias);
         Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, direction, orderBy);
